9b11d143d76455681dc9cddf47c4996c0fe91bce
inventivetalent <mail@inventivetalent.org>
2020-05-09 11:13:54 +0200

    add helper scripts

---
+++ helper/prerender.php
@@ -0,0 +1,11 @@
+<?php
+// PHP helper script to rewrite crawler requests to the SSR server
+include "prerender_config.php";
+
+$ch = curl_init(PRERENDER_HOST . "/render?token=" . PRERENDER_TOKEN . "&url=" . urlencode("https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$res = curl_exec($ch);
+$header = curl_getinfo($ch);
+curl_close($ch);
+
+echo $res;
---
+++ helper/prerender_config.php
@@ -0,0 +1,3 @@
+<?php
+define("PRERENDER_TOKEN", "12345");
+define("PRERENDER_HOST", "https://ssr.example.com");
