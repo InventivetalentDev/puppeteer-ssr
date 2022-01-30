41dd71f47a880714a203101bda2484bd18f7302b
inventivetalent <mail@inventivetalent.org>
2022-01-30 22:13:00 +0100

    .

--- helper/prerender.php
+++ helper/prerender.php
@@ -5,6 +5,7 @@ include "prerender_config.php";
 
 $ch = curl_init(PRERENDER_HOST . "/render?token=" . PRERENDER_TOKEN . "&url=" . urlencode("https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
 $res = curl_exec($ch);
 $header = curl_getinfo($ch);
 curl_close($ch);
