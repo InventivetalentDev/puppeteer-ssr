1605b68cb9af4834274c0eb42f059bbbb68da9e1
inventivetalent <mail@inventivetalent.org>
2022-12-13 16:04:22 +0100

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     apps: [{
         name: "ssr",
-        script: "index.js",
+        script: "dist/index.js",
         args: ["--color", "--time"],
         time: true,
-        interpreter: "node@14.15.4",
-        max_memory_restart: "300M"
+        interpreter: "node@19.2.0",
+        max_memory_restart: "300M",
+        env: {
+            PORT: 7462,
+            TOKEN: "12345",
+            GOOGLE_CHROME_BIN: ""
+        }
     }]
 }
