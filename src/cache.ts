019a22a46230985351c7fd9fe0ca409c8fab2129
inventivetalent <mail@inventivetalent.org>
2022-12-14 21:41:04 +0100

    log

--- src/cache.ts
+++ src/cache.ts
@@ -3,13 +3,20 @@ import { AsyncLoadingCache, AsyncMappingFunction } from "@inventivetalent/loadin
 import { Caches } from "@inventivetalent/loading-cache";
 import { Time } from "@inventivetalent/time";
 import * as redis from "./redis";
+import * as logging from "./logging";
 
 const cache: AsyncLoadingCache<string, string> = Caches.builder()
     .expireAfterWrite(Time.seconds(Number(process.env.MEMORY_CACHE_DURATION) || 60)) // seconds
     .recordStats(false)
     .buildAsync();
 
-async function getOr(url: string, mappingFunction: AsyncMappingFunction<string, string>) {
+async function getOr(url: string, mappingFunction: AsyncMappingFunction<string, string>): Promise<string> {
+    let cached = await cache.getIfPresent(url);
+    if (cached) {
+        logging.debug("got cached content for " + url);
+        return cached;
+    }
+
     if (process.env.REDISCLOUD_URL) {
         let originalMapping = mappingFunction;
         mappingFunction = async (key: string) => {
@@ -17,6 +24,7 @@ async function getOr(url: string, mappingFunction: AsyncMappingFunction<string, 
             try {
                 let redisValue = await redis.get(key);
                 if (redisValue) {
+                    logging.debug("got redis content for " + url);
                     return redisValue;
                 }
             } catch (e) {
@@ -33,7 +41,9 @@ async function getOr(url: string, mappingFunction: AsyncMappingFunction<string, 
             return value;
         };
     }
-    return cache.get(url, mappingFunction);
+
+    logging.debug("getting live content for " + url);
+    return await cache.get(url, mappingFunction);
 }
 
 
